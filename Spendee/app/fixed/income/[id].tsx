3b7421f82c5d7859986257fb8dbc34d0628f6d57
Jorge_N <valentinnave@gmail.com>
2026-02-10 13:22:26 -0300

    add fixed income detail page

--- Spendee/app/fixed/income/[id].tsx
+++ Spendee/app/fixed/income/[id].tsx
@@ -1,75 +1,93 @@
-import FixedExpenseModal from '@/components/FixedExpenseModal'
+import FixedIncomeModal from '@/components/FixedIncomeModal'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { Text } from '@/components/ui/text'
-import useFixedExpenseDetail from '@/hooks/fixed-expenses/useFixedExpenseDetail'
-import useFixedExpenses from '@/hooks/fixed-expenses/useFixedExpenses'
-import { getPaymentMethodIcon } from '@/lib/utils'
+import useFixedIncomeDetail from '@/hooks/fixed-incomes/useFixedIncomeDetail'
+import useFixedIncomes from '@/hooks/fixed-incomes/useFixedIncomes'
+import { Frequency } from '@/services/fixed-income.service'
 import { useLocalSearchParams } from 'expo-router'
-import { Calendar, Edit3, Tag, Trash2 } from 'lucide-react-native'
+import { DollarSign, Edit3, Repeat, Trash2 } from 'lucide-react-native'
 import React, { useState } from 'react'
 import { Alert, ScrollView, View } from 'react-native'
 
-export default function FixedExpenseDetailScreen() {
+export default function FixedIncomeDetailScreen() {
   const { id } = useLocalSearchParams()
-  const expenseId = Number(id)
+  const incomeId = Number(id)
 
-  const { fixedExpenseDetail: expense, isFixedExpenseLoading } =
-    useFixedExpenseDetail(expenseId)
-  const { deleteFixedExpense, editFixedExpense } = useFixedExpenses()
+  const { fixedIncomeDetail: income, isFixedIncomeLoading } =
+    useFixedIncomeDetail(incomeId)
+  const { deleteFixedIncome, editFixedIncome } = useFixedIncomes()
 
   const [isEditModalVisible, setIsEditModalVisible] = useState(false)
 
-  if (isFixedExpenseLoading)
+  if (isFixedIncomeLoading)
     return (
       <View className="flex-1 justify-center items-center">
         <Text>Cargando...</Text>
       </View>
     )
-  if (!expense)
+
+  if (!income)
     return (
       <View className="flex-1 justify-center items-center">
-        <Text>No se encontró el gasto</Text>
+        <Text>No se encontró el ingreso</Text>
       </View>
     )
 
-  const PaymentIcon = getPaymentMethodIcon(expense.metodoPago)
-
   const handleDelete = () => {
     Alert.alert(
-      'Eliminar Gasto',
-      '¿Estás seguro de que querés borrar este gasto fijo?',
+      'Eliminar Ingreso',
+      '¿Estás seguro de que querés borrar este ingreso fijo?',
       [
         { text: 'Cancelar', style: 'cancel' },
         {
           text: 'Eliminar',
           style: 'destructive',
-          onPress: () => deleteFixedExpense(expenseId),
+          onPress: () => deleteFixedIncome(incomeId),
         },
       ],
     )
   }
 
+  const frecuencia = income.frecuencia as unknown as keyof typeof Frequency
+  let valorMensualizado = 0
+
+  if (frecuencia === 'WEEKLY') {
+    valorMensualizado = income.ingreso * 4
+  } else if (frecuencia === 'YEARLY') {
+    valorMensualizado = income.ingreso / 12
+  } else {
+    valorMensualizado = income.ingreso
+  }
+
   return (
     <ScrollView className="flex-1 p-6">
       <View className="items-center my-8">
-        <Text className="text-muted-foreground mb-2">Monto mensual</Text>
+        <Text className="text-muted-foreground mb-2">Monto del ingreso</Text>
         <Text className="text-5xl font-bold text-primary">
-          $ {Number(expense.gasto).toLocaleString()}
+          $ {Number(income.ingreso).toLocaleString()}
         </Text>
       </View>
 
       <Card className="gap-4 p-6 rounded-[32px] border border-border">
-        <DetailRow icon={Tag} label="Nombre" value={expense.nombre} />
         <DetailRow
-          icon={Calendar}
-          label="Día de vencimiento"
-          value={`Día ${expense.diaDeVencimiento}`}
+          icon={DollarSign}
+          label="Monto Nominal"
+          value={`$ ${Number(income.ingreso).toLocaleString()}`}
+        />
+        {}
+        <DetailRow
+          icon={DollarSign}
+          label="Monto Mensual"
+          value={`$ ${Number(valorMensualizado).toLocaleString()}`}
         />
         <DetailRow
-          icon={PaymentIcon}
-          label="Método de pago"
-          value={expense.metodoPago}
+          icon={Repeat}
+          label="Frecuencia"
+          value={
+            Frequency[income.frecuencia as unknown as keyof typeof Frequency] ||
+            income.frecuencia
+          }
           isCapitalized
         />
       </Card>
@@ -93,15 +111,15 @@ export default function FixedExpenseDetailScreen() {
         </Button>
       </View>
 
-      <FixedExpenseModal
+      <FixedIncomeModal
         visible={isEditModalVisible}
         onClose={() => setIsEditModalVisible(false)}
         isSubmitting={false}
+        initialData={income}
         onSubmit={(data) => {
-          editFixedExpense({ id: expenseId, data: data })
+          editFixedIncome({ id: incomeId, data: data })
           setIsEditModalVisible(false)
         }}
-        initialData={expense}
       />
     </ScrollView>
   )
